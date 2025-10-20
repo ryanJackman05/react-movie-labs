c2aa7077db4e5253c2fe7b60b68dbf79e55ae5b1
ryanJackman05 <20108256@mail.wit.ie>
2025-10-21 00:12:06 +0100

    Refactor: Template list page; HTTP API calls centralized.

--- movies/src/pages/movieDetailsPage.jsx
+++ movies/src/pages/movieDetailsPage.jsx
@@ -5,6 +5,7 @@ import Grid from "@mui/material/Grid";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import { useParams } from 'react-router';
+import { getMovie, getMovieImages } from "../api/tmdb-api";
 
 const MoviePage = (props) => {
   const { id } = useParams();
@@ -12,30 +13,18 @@ const MoviePage = (props) => {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    fetch( // Use the movie endpoint to access full details, passing in key to api-key as parameter
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((movie) => {
-        // console.log(movie)
-        setMovie(movie);
-      });
+    getMovie(id).then((movie) => {
+      setMovie(movie);
+    });
   }, [id]);
 
   useEffect(() => {
-    fetch( // Use the movie directory to access movie's images endpoint, passing in key to api-key as parameter
-      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => json.posters)
-      .then((images) => {
-        // console.log(images)
-        setImages(images);
-      });
-      // eslint-disable-next-line
+    getMovieImages(id).then((images) => {
+      setImages(images);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
 
 
   return (
