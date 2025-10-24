05b79c7efeef47f402a7eba33c5caa118c252bc5
ryanJackman05 <20108256@mail.wit.ie>
2025-10-24 13:26:55 +0100

    Exercise: Movie Details Page- included production countries.

--- movies/src/components/movieDetails/index.jsx
+++ movies/src/components/movieDetails/index.jsx
@@ -60,6 +60,12 @@ const [drawerOpen, setDrawerOpen] = useState(false);
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
+      <Paper component="ul" sx={{...root}}>
+        <Chip label="Production Countries" sx={{...chip}} color="primary" />
+        {movie.production_countries.map((g) => (
+          <Chip label={g.name} sx={{...chip}} />
+        ))}
+      </Paper>
       <Fab
         color="secondary"
         variant="extended"
